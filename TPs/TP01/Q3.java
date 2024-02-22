7372e92c887ddd18175c9a33b2b1f4d46151d52e
CaioFD <caiodiniz200204@gmail.com>
2024-02-22 16:02:28 -0300

    ..

--- TPs/TP01/Q3.java
+++ TPs/TP01/Q3.java
@@ -1,41 +1,12 @@
-// package TPs.TP01;
-
-/**
- * Q3
- */
-/* 
-public class Q3 {
-    public static String JC(String palavra){
-        String temporaria = "";
-        for(int i= 0; i < palavra.length(); i++){
-            temporaria += (char) ((int) palavra.charAt(i)+3);
-        }
-        return temporaria;
-     }
-    public static void main(String[] args) {
-        while (true) {
-            String palavra = "";
-            palavra = MyIO.readLine();
-            if (palavra.charAt(0)== 'F' && palavra.charAt(1)== 'I' && palavra.charAt(2)== 'M') {
-                System.exit(0);
-            }else{
-                palavra = JC(palavra);
-                MyIO.println(palavra);
-            }
-        }
-    }
-}
-*/
-// import java.util.Scanner;
 
 public class Q3 {
 
-    public static boolean isFim( String s )
+    public static boolean isFim( String palavra )
     {
-        return( s.charAt(0) == 'F' && s.charAt(1) == 'I' && s.charAt(2) == 'M' );
+        return( palavra.charAt(0) == 'F' && palavra.charAt(1) == 'I' && palavra.charAt(2) == 'M' );
     } // end isfim
 
-    public static String cifrar(String pala) {
+    public static String JC(String pala) {
         String temp = "";
         for (int i = 0; i < pala.length(); i++) {
             temp += (char) ((int) pala.charAt(i) + 3);
@@ -45,14 +16,14 @@ public class Q3 {
 
     public static void main(String[] args) {
 
-        String s = "";
+        String palavra = "";
 
         do {
 
-            s = MyIO.readLine();
-            s = cifrar(s);
-            System.out.println(s);
+            palavra = MyIO.readLine();
+            palavra = JC(palavra);
+            System.out.println(palavra);
         
-        } while ( !isFim( s ) );
+        } while ( !isFim( palavra ) );
     } // end main
 } // end class
